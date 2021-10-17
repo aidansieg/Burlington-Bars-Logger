2d7698b915926774be03abda40df71f65ca63fd6
Aidan Siegel <55719802+aidansieg@users.noreply.github.com>
2021-10-17 13:35:52 -0400

    Add files via upload

---
+++ Burlington-Bar-Logger/Burlington-Bar-Logger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Burlington-Bar-Logger
+//
+//  Created by Aidan Siegel on 10/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
